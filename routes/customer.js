73e6638323b816e6abaf9eeaa61aa3f8b2402f7f
Aldrin Quitalig <aldrin_quitalig@yahoo.com>
2024-11-05 12:29:03 +0800

    SeaSide

--- routes/customer.js
+++ routes/customer.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const router= express.Router();
 
-// Customers
+// Customers123
 router.get('/', (request, response) => 
 {
     response.status(200).json({ message: 'GET all ' });
